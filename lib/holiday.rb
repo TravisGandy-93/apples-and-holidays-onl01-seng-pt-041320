61b666bc6b36d0e6862911450360064b5036750d
Travis Gandy <travis_gandy@yahoo.com>
2020-04-25 00:06:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,31 +18,38 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+  holiday_hash[:winter][:christmas] << "#{supply}"
+  holiday_hash[:winter][:new_years] << "#{supply}"
+  holiday_hash
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  holiday_hash[:spring][:memorial_day] << "#{supply}"
+  holiday_hash
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+  holiday_hash[season][holiday_name] = supply_array
+  holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+ winter_supply_one = holiday_hash[:winter][:christmas]
+ winter_supply_two = holiday_hash[:winter][:new_years]
+ winter_supply_one + winter_supply_two
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -53,6 +60,20 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  holiday_hash.each {
+    |key1, key2|  
+    puts "#{key1.capitalize}:"
+      key2.each {
+        |day, supply| 
+          if day == :new_years || day == :fourth_of_july || day == :memorial_day
+         
+            puts " #{day.capitalize.split('_').join(" ")}: #{supply.join(", ")}"
+           else 
+               puts "  #{day.capitalize}: #{supply.join(", ")}"
+        end 
+      }
+  }
+    
 
 end
 
